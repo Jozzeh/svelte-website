5397f9aacf8218e7588e5a13de49400ebe058e87
Jos De Berdt <josdeberdt@MacBook-Pro-van-Jos.local>
2020-05-13 12:40:33 +0200

    Copy - Link to real website

--- src/components/home/More.svelte
+++ src/components/home/More.svelte
@@ -31,16 +31,29 @@
         </p>
         <p>
           That includes using HTML &amp; CSS but also on-page SEO, CI/CD
-          pipelines or other languages such as Flutter / Dart. 
+          pipelines or other languages such as Flutter / Dart.
           <br />
           When I hear or read some rant about CSS and how bad it is; I can't
           find myself in the reasoning behind it. I believe CSS is fantastic
           language.
           <br />
-          Call me old school but I like to seperate lay-out from behavior...<br/><br/>
+          Call me old school but I like to seperate lay-out from behavior...
+          <br />
+          <br />
           The reason I'm still in development after all these years?
-          <strong>Adaptability</strong><br/>The development industry is moving at a frantic pace, and developers need to keep on learning.<br/>
-          <span class="small">(This website was created with Sapper - a svelte framework. Check my github repo's for the code.)</span>
+          <strong>Adaptability</strong>
+          <br />
+          The development industry is moving at a frantic pace, and developers
+          need to keep on learning.
+          <br />
+          <span class="small">
+            (This website was created with Sapper - a svelte framework - as a
+            test. Check my github repo's for the code.)
+          </span>
+          <br />
+          <span class="small">
+            <a href="https://www.josdeberdt.be">Go to the real website...</a>
+          </span>
         </p>
       </div>
     </div>
